6791134f962060614f82b68d6328535c5cc4af57
praveshgiri <prav3shhh@gmail.com>
2019-08-12 14:02:25 -0400

    check

--- QuizApp/Model/QuestionBank.cs
+++ QuizApp/Model/QuestionBank.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 
@@ -15,7 +15,7 @@ namespace QuizApp.Model
         {
             new Question { QuestionText = "'The Hurt Locker' won the Academy Award for Best picture in 2009", CorrectAnswer = true},
             new Question { QuestionText = "Gone with the wind was the first movie to win the Oscar for Best picture", CorrectAnswer = false},
-            new Question { QuestionText = "LA LA LA LAND was entirely filmed in Vancouver", CorrectAnswer = false},
+            new Question { QuestionText = "LA filmed in Vancouver", CorrectAnswer = false},
             new Question { QuestionText = "Superman made his first movie apperance in the 1940s", CorrectAnswer = true},
             new Question { QuestionText = "Tom Hanks speaks less than 200 words in Toy Story 2 ", CorrectAnswer = false},
             new Question { QuestionText = "Paul Newman starred in the 1961 movie The Hustler", CorrectAnswer = true},
