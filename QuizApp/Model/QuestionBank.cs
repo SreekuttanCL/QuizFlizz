d1ae57cd087ad03868dce18a57fd9bd067f36752
arjunneupane <arjun.neupane128@gmail.com>
2019-08-12 14:31:12 -0400

    Arjun did this

--- QuizApp/Model/QuestionBank.cs
+++ QuizApp/Model/QuestionBank.cs
@@ -17,7 +17,7 @@ namespace QuizApp.Model
             new Question { QuestionText = "Gone with the wind was the first movie to win the Oscar for Best picture", CorrectAnswer = false},
             new Question { QuestionText = "LA filmed in Vancouver", CorrectAnswer = false},
             new Question { QuestionText = "Batman made his first movie apperance in the 1940s", CorrectAnswer = false},
-            new Question { QuestionText = "Tom Hanks speaks less than 200 words in Toy Story 2 ", CorrectAnswer = false},
+            new Question { QuestionText = "Arjun Hanks speaks less than 200 words in Toy Story 2 ", CorrectAnswer = false},
             new Question { QuestionText = "Paul Newman starred in the 1961 movie The Hustler", CorrectAnswer = true},
             new Question { QuestionText = "Academy Award or Oscar was first presented in 1929", CorrectAnswer = true},
             new Question { QuestionText = "Judy Garland did not star as Dorothy Gale in The Wizard of Oz", CorrectAnswer = false},
